dbd1481f5e70a98aecd256b5fa043a1cdaebb4a5
Hamid Zare <dellydela@gmail.com>
2019-02-21 22:04:54 -0600

    pwa; pwabuilder

--- assets/main.js
+++ assets/main.js
@@ -33,3 +33,19 @@ function spaceOut(parent) {
 
 // fix spacing for all .manheader
 Array.from(document.querySelectorAll('.manheader')).forEach(spaceOut);
+
+
+// **** service worker pwa
+
+//This is the "Offline copy of pages" service worker
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
